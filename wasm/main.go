7161839fbbd81463f50c3691693d5c4a87680d5c
Matteo Gioioso <gioioso.matteo@gmail.com>
2023-08-15 10:47:35 +0300

    improve wasm error handling

--- wasm/main.go
+++ wasm/main.go
@@ -15,21 +15,31 @@ func main() {
 	<-make(chan bool)
 }
 
+func marshalError(expErr pkg.ExplainedError) string {
+	marshal, err := json.Marshal(expErr)
+	if err != nil {
+		panic(err)
+	}
+	return string(marshal)
+}
+
 func explain() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) (ret any) {
 		defer func() {
 			if r := recover(); r != nil {
 				ret = map[string]any{
-					"error":         "query explain panic",
-					"error_details": fmt.Errorf("%s", r).Error(),
-					"error_stack":   string(debug.Stack()),
+					"error": marshalError(pkg.ExplainedError{
+						Error:   "query explain panic",
+						Details: fmt.Errorf("%s", r).Error(),
+						Stack:   string(debug.Stack()),
+					}),
 				}
 			}
 		}()
 
 		if len(args) != 1 {
 			return map[string]any{
-				"error": "invalid no of arguments passed",
+				"error": pkg.ExplainedError{Error: "invalid no of arguments passed"},
 			}
 		}
 
@@ -37,8 +47,10 @@ func explain() js.Func {
 		node, err := pkg.GetRootNodeFromPlans(plans)
 		if err != nil {
 			return map[string]any{
-				"error":         "invalid input: the plan was probably not valid JSON or text",
-				"error_details": err.Error(),
+				"error": marshalError(pkg.ExplainedError{
+					Error:   "invalid input: the plan was probably not valid JSON or text",
+					Details: err.Error(),
+				}),
 			}
 		}
 
@@ -47,8 +59,10 @@ func explain() js.Func {
 		statsGather := pkg.NewStatsGather()
 		if err := statsGather.GetStatsFromPlans(plans); err != nil {
 			return map[string]any{
-				"error":         "could not get stats from plan",
-				"error_details": err.Error(),
+				"error": marshalError(pkg.ExplainedError{
+					Error:   "could not get stats from plan",
+					Details: err.Error(),
+				}),
 			}
 		}
 
@@ -74,8 +88,10 @@ func explain() js.Func {
 		marshalledExplained, err := json.Marshal(explained)
 		if err != nil {
 			return map[string]any{
-				"error":         "could not marshal enriched plan",
-				"error_details": err.Error(),
+				"error": marshalError(pkg.ExplainedError{
+					Error:   "could not marshal enriched plan",
+					Details: err.Error(),
+				}),
 			}
 		}
 
